e9381fed613104341b3718f0edd68c0546393e49
juni <tmdwns0927@naver.com>
2025-04-16 10:53:53 +0900

    styled-components Example

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,68 @@
+import styled from "styled-components";
+
+/**
+ * .content-container {
+  @include flex($direction: column, $align: flex-start, $gap: 5px);
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    @include font($size: 12px);
+    color: $tag-color;
+    border: 1px solid $tag-color;
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    @include font($size: 18px, $weight: 600);
+  }
+  p {
+    @include font($size: 12px);
+    color: $gray;
+  }
+}
+
+ */
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 5px;
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    font-size: 12px;
+    color: $tag-color;
+    border: 1px solid $tag-color;
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    font-size: 18px;
+    font-weight: 600;
+  }
+  p {
+    font-size: 12px;
+    color: gray;
+  }
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <ContentContainer>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </ContentContainer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,40 @@
+import styled from "styled-components";
 export default function Header() {
+  const Header = styled.header`
+    background-color: black;
+    padding: 20px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const H1 = styled.h1`
+    font-size: 32px;
+    color: white;
+  `;
+
+  const MenuContainer = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  `;
+
+  const MenuItem = styled.li`
+    font-size: 16px;
+    font-weight: 400;
+    list-style: none;
+    color: white;
+    list-style: none;
+  `;
   return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
+    <Header>
+      <H1>OZ 코딩스쿨</H1>
+      <MenuContainer>
+        <MenuItem>로그인</MenuItem>
+        <MenuItem>회원가입</MenuItem>
+        <MenuItem>내클래스</MenuItem>
+      </MenuContainer>
+    </Header>
   );
 }
